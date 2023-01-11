129d067033590541927a44b91ff8eb59f0635dd3
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-01-11 22:43:22 +0300

    Added datas for database

--- VehicleAPI.DataAccess/Concrete/Context/VehicleAPIDbContext.cs
+++ VehicleAPI.DataAccess/Concrete/Context/VehicleAPIDbContext.cs
@@ -17,6 +17,110 @@ public class VehicleAPIDbContext : DbContext
                 warnings.Ignore(CoreEventId.LazyLoadOnDisposedContextWarning);
             });
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+        #region Creation of Car
+        var Car1 = new CarModel()
+		{
+			Id = 1,
+            Headlights = true,
+            Color = "red",
+            Wheels = 2
+		};
+        var Car2 = new CarModel()
+		{
+			Id = 2,
+            Headlights = true,
+            Color = "blue",
+            Wheels = 3
+		};
+        var Car3= new CarModel()
+		{
+			Id = 3,
+            Headlights = true,
+            Color = "black",
+            Wheels = 4
+		};
+        var Car4 = new CarModel()
+		{
+			Id = 4,
+            Headlights = true,
+            Color = "white",
+            Wheels = 5
+		};
+        modelBuilder.Entity<CarModel>().HasData(Car1,Car2,Car3,Car4);
+		base.OnModelCreating(modelBuilder);
+        #endregion
+
+        #region Creation of Boat
+        var Boat1 = new BoatModel()
+		{
+			Id = 1,
+            Capacity = 1000,
+            Color = "red"
+		};
+        var Boat2 = new BoatModel()
+		{
+			Id = 2,
+            Capacity = 100,
+            Color = "blue"
+		};
+        var Boat3 = new BoatModel()
+		{
+			Id = 3,
+            Capacity = 40,
+            Color = "black"
+		};
+        var Boat4 = new BoatModel()
+		{
+			Id = 4,
+            Capacity = 50,
+            Color = "white"
+		};
+        
+		modelBuilder.Entity<BoatModel>().HasData(Boat1,Boat2,Boat3,Boat4);
+		base.OnModelCreating(modelBuilder);
+
+        #endregion
+
+        #region Creation of Bus
+        var Bus1 = new BusModel()
+		{
+			Id = 1,
+            LuggageCapacity = 50,
+            Color = "red",
+            Wheels = 2,
+
+		};
+         var Bus2 = new BusModel()
+		{
+			Id = 2,
+            LuggageCapacity = 30,
+            Color = "blue",
+            Wheels = 2,
+
+		};
+         var Bus3 = new BusModel()
+		{
+			Id = 3,
+            LuggageCapacity = 20,
+            Color = "black",
+            Wheels = 2,
+
+		};
+         var Bus4 = new BusModel()
+		{
+			Id = 4,
+            LuggageCapacity = 25,
+            Color = "white",
+            Wheels = 2,
+
+		};
+        
+		modelBuilder.Entity<BusModel>().HasData(Bus1,Bus2,Bus3,Bus4);
+		base.OnModelCreating(modelBuilder);
+        #endregion
+    }
 
     public DbSet<BoatModel> Boats { get; set; }
     public DbSet<BusModel> Buses { get; set; }
---
+++ VehicleAPI.DataAccess/Migrations/20230111194149_Mig-5.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VehicleAPI.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Boats",
+                columns: new[] { "Id", "Capacity", "Color" },
+                values: new object[,]
+                {
+                    { 1, 1000, "red" },
+                    { 2, 100, "blue" },
+                    { 3, 40, "black" },
+                    { 4, 50, "white" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Buses",
+                columns: new[] { "Id", "Color", "LuggageCapacity", "Wheels" },
+                values: new object[,]
+                {
+                    { 1, "red", 50, 2 },
+                    { 2, "blue", 30, 2 },
+                    { 3, "black", 20, 2 },
+                    { 4, "white", 25, 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "Color", "Headlights", "Wheels" },
+                values: new object[,]
+                {
+                    { 1, "red", true, 2 },
+                    { 2, "blue", true, 3 },
+                    { 3, "black", true, 4 },
+                    { 4, "white", true, 5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Boats",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Boats",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Boats",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Boats",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Buses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Buses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Buses",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Buses",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
